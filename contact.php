5ddec0a543fd636fbebe600d351382484ae563b1
GulaminHalim <hotgrill.gmv@gmail.com>
2024-10-21 05:16:06 +0700

    fix contact.php

--- contact.php
+++ contact.php
@@ -59,19 +59,22 @@
   // Create connection
   $conn = mysqli_connect($servername, $username, $password, $dbname);
   // Check connection
-  if (!$conn) {
+  /*if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   }
-
-
+*/
   $sql = "INSERT INTO datauser (Username, Email, Subject, Message)
-  VALUES ($name, $email, $subject, $message)";
+VALUES ('$name', '$email', '$subject', '$message')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Your message has been sent. Thank you!";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+
   
-  if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
   
   
   //mysqli_close($conn);
